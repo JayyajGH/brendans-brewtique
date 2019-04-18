e4fb6f4cd6a2d35cedf60e83e60b3b9bb5c73d9a
Jason Harris <jason.harris@endsleigh.co.uk>
2019-04-18 13:58:04 +0100

    Sort out the spacing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<main class="wrapper" id="main">
-	<section>
+		<section>
 			<h1 class="heading1 heading--dark">
 				<span class="heading--item">TAPROOM</span>
 				<span class="heading--spacer">.</span>
@@ -20,43 +20,43 @@
 					You are free to enjoy any of our great beers and ciders on or off the premises.
 				</p>
 			</div>
-	</section>
+		</section>
 
-	<section class="pair pair--dark u-margin-bottom--large" id="about">
-		<div class="image-section image--brendan"></div>
-		<div class="image-text">
-			<h2 class="heading2 heading--light">About</h2>
-			<p class="u-margin-bottom--medium">
-				Bristol is very fortunate to have some of the best breweries in the country.  Unfortunately, Bristol has had a shortage of places where you can buy beer from breweries across Bristol, the UK, Europe and beyond - all under one roof.
-			</p>
-			<p class="u-margin-bottom--medium">
-				Brendan's Brewtique is the solution to this problem.
-			</p>
-		</div>
-	</section>
+		<section class="pair pair--dark u-margin-bottom--large" id="about">
+			<div class="image-section image--brendan"></div>
+			<div class="image-text">
+				<h2 class="heading2 heading--light">About</h2>
+				<p class="u-margin-bottom--medium">
+					Bristol is very fortunate to have some of the best breweries in the country.  Unfortunately, Bristol has had a shortage of places where you can buy beer from breweries across Bristol, the UK, Europe and beyond - all under one roof.
+				</p>
+				<p class="u-margin-bottom--medium">
+					Brendan's Brewtique is the solution to this problem.
+				</p>
+			</div>
+		</section>
 
-	<section class="pair u-margin-bottom--large" id="events">
-		<div class="image-text">
-			<h2 class="heading2 heading--dark">Events</h2>
-			<p class="u-margin-bottom--medium">
-				We regularly hold tasting events with various breweries and beer experts.  In the past we've had events with breweries like Moor and Arbor.
-			</p>
-			<p class="u-margin-bottom--medium">
-				Our next event is an introduction to Belgian beer with Tim Webb, author of the Good Beer Guide Belgium, on 25th April 2019.
-			</p>
-			<a href="https://www.eventbee.com/v/brewtique/event?eid=168576862#/tickets" target="_blank" rel="noopener noreferrer" class="button button--primary body--bold">Book tickets</a>
-		</div>
-		<div class="image-section image--tasting"></div>
-	</section>
+		<section class="pair u-margin-bottom--large" id="events">
+			<div class="image-text">
+				<h2 class="heading2 heading--dark">Events</h2>
+				<p class="u-margin-bottom--medium">
+					We regularly hold tasting events with various breweries and beer experts.  In the past we've had events with breweries like Moor and Arbor.
+				</p>
+				<p class="u-margin-bottom--medium">
+					Our next event is an introduction to Belgian beer with Tim Webb, author of the Good Beer Guide Belgium, on 25th April 2019.
+				</p>
+				<a href="https://www.eventbee.com/v/brewtique/event?eid=168576862#/tickets" target="_blank" rel="noopener noreferrer" class="button button--primary body--bold">Book tickets</a>
+			</div>
+			<div class="image-section image--tasting"></div>
+		</section>
 
-	<section class="pair pair--dark u-margin-bottom--large" id="ontap">
-		<div class="image-section image--taps"></div>
-		<div class="image-text">
-			<h2 class="heading2 heading--light">What's on tap?</h2>
-			<tap-list></tap-list>
-		</div>
-	</section>
-</main>
+		<section class="pair pair--dark u-margin-bottom--large" id="ontap">
+			<div class="image-section image--taps"></div>
+			<div class="image-text">
+				<h2 class="heading2 heading--light">What's on tap?</h2>
+				<tap-list></tap-list>
+			</div>
+		</section>
+	</main>
 </template>
 
 <script>
