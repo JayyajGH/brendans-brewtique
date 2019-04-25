9ba35f401a7d364b4f36728c107ad9413bd109c5
Jason Harris <jason.harris@endsleigh.co.uk>
2019-04-25 11:36:38 +0100

    Tidy up the main page CSS

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -22,9 +22,9 @@
 			</div>
 		</section>
 
-		<section class="pair pair--dark u-margin-bottom--large" id="about">
-			<div class="image-section image--brendan"></div>
-			<div class="image-text">
+		<section class="image-text__panel image-text__panel--dark u-margin-bottom--large" id="about">
+			<div class="image-text__image image--brendan"></div>
+			<div class="image-text__text">
 				<h2 class="heading2 heading--light">About</h2>
 				<p class="u-margin-bottom--medium">
 					Bristol is very fortunate to have some of the best breweries in the country.  Unfortunately, Bristol has had a shortage of places where you can buy beer from breweries across Bristol, the UK, Europe and beyond - all under one roof.
@@ -35,8 +35,8 @@
 			</div>
 		</section>
 
-		<section class="pair u-margin-bottom--large" id="events">
-			<div class="image-text">
+		<section class="image-text__panel u-margin-bottom--large" id="events">
+			<div class="image-text__text">
 				<h2 class="heading2 heading--dark">Events</h2>
 				<p class="u-margin-bottom--medium">
 					We regularly hold tasting events with various breweries and beer experts.  In the past we've had events with breweries like Moor and Arbor.
@@ -46,12 +46,12 @@
 				</p>
 				<a href="https://www.eventbee.com/v/brewtique/event?eid=168576862#/tickets" target="_blank" rel="noopener noreferrer" class="button button--primary body--bold">Book tickets</a>
 			</div>
-			<div class="image-section image--tasting"></div>
+			<div class="image-text__image image--tasting"></div>
 		</section>
 
-		<section class="pair pair--dark u-margin-bottom--large" id="ontap">
-			<div class="image-section image--taps"></div>
-			<div class="image-text">
+		<section class="image-text__panel image-text__panel--dark u-margin-bottom--large" id="ontap">
+			<div class="image-text__image image--taps"></div>
+			<div class="image-text__text">
 				<h2 class="heading2 heading--light">What's on tap?</h2>
 				<tap-list></tap-list>
 			</div>
@@ -68,94 +68,93 @@
 </script>
 
 <style>
-.wrapper--small {
-  padding-left: 10px;
-  padding-right: 10px;
-  margin: 0 auto;
-}
+	.wrapper--small {
+	  padding-left: 10px;
+	  padding-right: 10px;
+	  margin: 0 auto;
+	}
 
-@media (min-width: 50em) {
-  .wrapper--small {
-		max-width: 600px;
-    padding-left: 0;
-    padding-right: 0;
-  }
-}
+	@media (min-width: 50em) {
+	  .wrapper--small {
+			max-width: 600px;
+	    padding-left: 0;
+	    padding-right: 0;
+	  }
+	}
 
-.heading--item {
-  padding-left: 10px;
-  padding-right: 10px;
-}
+	.heading--item {
+	  padding-left: 10px;
+	  padding-right: 10px;
+	}
 
-.heading--spacer {
-  vertical-align: text-top;
-}
+	.heading--spacer {
+	  vertical-align: text-top;
+	}
 
-.pair {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-}
+	.image-text__panel {
+	  display: flex;
+	  flex-wrap: wrap;
+	  justify-content: space-between;
+	}
 
-.pair--dark {
-  background-color: var(--grey-base);
-  color: var(--white);
-}
+	.image-text__panel--dark {
+	  background-color: var(--grey-base);
+	  color: var(--white);
+	}
 
+	.image-text__text {
+	  width: 100%;
+	  padding: 10px;
+	}
 
-.image-text {
-  width: 100%;
-  padding: 10px;
-}
+	@media (min-width: 50em) {
+	  .image-text__text {
+	    width: 50%;
+	  }
+	}
 
-@media (min-width: 50em) {
-  .image-text {
-    width: 50%;
-  }
-}
+	.image-text__image {
+		width: 100%;
+	}
 
-.image--brendan {
-  background-image: url('~assets/images/brendan.png');
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 400px;
-}
+	@media (min-width: 50em) {
+		.image-text__image {
+			width: 50%;
+		}
+	}
 
-.image-section {
-  width: 100%;
-}
+	.image--brendan {
+	  background-image: url('~assets/images/brendan.png');
+	  background-repeat: no-repeat;
+	  background-size: cover;
+	  height: 400px;
+	}
 
-@media (min-width: 50em) {
-  .image-section {
-    width: 50%;
-  }
-}
+	.image--tasting {
+	  display: none;
+	}
 
-.image--tasting {
-  display: none;
-}
+	@media (min-width: 50em) {
+	  .image--tasting {
+	    background-image: url('~assets/images/tasting.jpg');
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	    height: 400px;
+	    display: block;
+	  }
+	}
 
-@media (min-width: 50em) {
-  .image--tasting {
-    background-image: url('~assets/images/tasting.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    height: 400px;
-    display: block;
-  }
-}
+	.image--taps {
+	  display: none;
+	}
 
-.image--taps {
-  display: none;
-}
-
-@media (min-width: 50em) {
-  .image--taps {
-    background-image: url('~assets/images/taps.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    height: 400px;
-    display: block;
-  }
-}
+	@media (min-width: 50em) {
+	  .image--taps {
+	    background-image: url('~assets/images/taps.jpg');
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	    height: 400px;
+	    display: block;
+	  }
+	}
 </style>
