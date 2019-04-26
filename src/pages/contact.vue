421ff8cfd02dff4a7e40477dfda4d39ec2cb0b02
Jason Harris <jason.harris@endsleigh.co.uk>
2019-04-26 11:00:15 +0100

    Add a custom error page

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -95,14 +95,6 @@
 </script>
 
 <style scoped lang="scss">
-  .link {
-    color: var(--orange-dark);
-  }
-
-  .link:hover {
-    text-decoration: underline;
-  }
-
   .contact__details {
     display: flex;
     flex-wrap: wrap;
