f0895406ad9ea6ec64f7cd7c0793a849b9133547
Jason Harris <jason.harris@endsleigh.co.uk>
2019-05-17 12:02:28 +0100

    Add a test for the RouterLink component

--- src/components/site-header/site-header.spec.js
+++ src/components/site-header/site-header.spec.js
@@ -16,3 +16,7 @@ test('it renders the correct initial markup', (t) => {
   // Check that the component is at least created in it's most basic sense
   t.true(wrapper.is('header'));
 });
+
+test("href should exist on generated contact link", (t) => {
+  t.is(wrapper.find(RouterLinkStub).props("to"), "/contact");
+})
