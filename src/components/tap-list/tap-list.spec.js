dd40b4513d67efa5cb458e764835b32bb7240ece
Jason Harris <jason.harris@endsleigh.co.uk>
2019-05-20 14:03:36 +0100

    Add tests for the taplist component

--- src/components/tap-list/tap-list.spec.js
+++ src/components/tap-list/tap-list.spec.js
@@ -1,14 +1,57 @@
 import test from 'ava';
 import { shallowMount } from '@vue/test-utils';
+import flushPromises from 'flush-promises';
 import TapList from './tap-list';
 
-let wrapper;
+const getTapListDetailsForVenueFactory = (tapList) => {
+  return async function() {
+    return {
+      taplistDetails: tapList
+    };
+  }
+}
 
-test.beforeEach(() => {
-  wrapper = shallowMount(TapList);
+const tapListFactory = (getTapListDetailsForVenue) => {
+  return shallowMount(TapList, {
+    methods: {
+      getTapListDetailsForVenue
+    }
+  });
+}
+
+test('it renders the correct initial markup when taplist details are present', async t => {
+  // Check that the component is at least created in it's most basic sense
+
+  const tapListDetails = {
+    beverageList: [
+        { beverageName: 'Even Sharks Need Water',
+          breweryName: 'Verdant',
+          beverageStyle: 'IPA',
+          ABV: 'Unknown'
+        }
+    ]
+  };
+
+  let getTapList = getTapListDetailsForVenueFactory(tapListDetails);
+  let wrapper = tapListFactory(getTapList);
+
+  await flushPromises();
+
+  t.true(wrapper.is('ul'));
 });
 
-test('it renders the correct initial markup', (t) => {
+
+test('it renders the correct initial markup when taplist details are missing', async t => {
   // Check that the component is at least created in it's most basic sense
-  //t.true(wrapper.is('footer'));
+
+  const tapListDetails = {
+    beverageList: []
+  };
+
+  let getTapList = getTapListDetailsForVenueFactory(tapListDetails);
+  let wrapper = tapListFactory(getTapList);
+
+  await flushPromises();
+
+  t.true(wrapper.is('p'));
 });
