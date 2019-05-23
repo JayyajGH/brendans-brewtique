50d34ecaabdadd7d924b511d9f07bb1ba9c650c7
Jason Harris <jason.harris@endsleigh.co.uk>
2019-05-23 15:23:23 +0100

    Add some tests to the contact form

--- src/components/send-message/send-message.spec.js
+++ src/components/send-message/send-message.spec.js
@@ -12,3 +12,17 @@ test('it renders the correct initial markup', (t) => {
   // Check that the component is at least created in it's most basic sense
   t.true(wrapper.is('div'));
 });
+
+test('it doesn\'t render any error messages on form load', (t) => {
+  t.is(wrapper.findAll('.form-field__error').length, 0);
+});
+
+test('it renders the correct error messages when missing form data', (t) => {
+  const submitButton = wrapper.find("input[type='submit']");
+  submitButton.trigger('click');
+
+  t.is(wrapper.findAll('.form-field__error').length, 3);
+  t.is(wrapper.findAll('#name-error').length, 1);
+  t.is(wrapper.findAll('#email-error').length, 1);
+  t.is(wrapper.findAll('#message-error').length, 1);
+});
