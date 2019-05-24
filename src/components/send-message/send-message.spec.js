2bdc3f09f8c308c0c90ad00d59fe28ee28b03e5f
Jason Harris <jason.harris@endsleigh.co.uk>
2019-05-24 15:16:13 +0100

    Add some more tests

--- src/components/send-message/send-message.spec.js
+++ src/components/send-message/send-message.spec.js
@@ -2,22 +2,19 @@ import test from 'ava';
 import { shallowMount } from '@vue/test-utils';
 import SendMessage from './send-message';
 
-let wrapper;
-
-test.beforeEach(() => {
-  wrapper = shallowMount(SendMessage);
-});
-
 test('it renders the correct initial markup', (t) => {
   // Check that the component is at least created in it's most basic sense
+  let wrapper = shallowMount(SendMessage);
   t.true(wrapper.is('div'));
 });
 
 test('it doesn\'t render any error messages on form load', (t) => {
+  let wrapper = shallowMount(SendMessage);
   t.is(wrapper.findAll('.form-field__error').length, 0);
 });
 
 test('it renders the correct error messages when missing form data', (t) => {
+  let wrapper = shallowMount(SendMessage);
   const submitButton = wrapper.find("input[type='submit']");
   submitButton.trigger('click');
 
@@ -25,4 +22,30 @@ test('it renders the correct error messages when missing form data', (t) => {
   t.is(wrapper.findAll('#name-error').length, 1);
   t.is(wrapper.findAll('#email-error').length, 1);
   t.is(wrapper.findAll('#message-error').length, 1);
+});
+
+test('it correctly validates an email address', (t) => {
+  let wrapper = shallowMount(SendMessage);
+  wrapper.setData({contactEmail: 'bob.smith@gmail.com'});
+  t.true(wrapper.vm.isEmailAddressValid());
+});
+
+test('it correctly detects an invalid email address when component is created', (t) => {
+  let wrapper = shallowMount(SendMessage);
+  t.false(wrapper.vm.isEmailAddressValid());
+});
+
+test('it correctly detects an invalid email address', (t) => {
+  let wrapper = shallowMount(SendMessage);
+  wrapper.setData({contactEmail: ''});
+  t.false(wrapper.vm.isEmailAddressValid());
+
+  wrapper.setData({contactEmail: 'bob'});
+  t.false(wrapper.vm.isEmailAddressValid());
+
+  wrapper.setData({contactEmail: 'bob.smith'});
+  t.false(wrapper.vm.isEmailAddressValid());
+
+  wrapper.setData({contactEmail: 'bob@smith'});
+  t.false(wrapper.vm.isEmailAddressValid());
 });
