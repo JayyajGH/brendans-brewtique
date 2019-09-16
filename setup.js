cf4ba6cad4c228f042261edface9ce127c5db501
Jason Harris <jayyaj@hotmail.com>
2019-09-16 16:35:54 +0100

    Update the comment

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
 const hooks = require('require-extension-hooks');
 
-// Set up a virtual browser environment.  This uses jsdom but jsdom-global could be used instead
+// Set up a virtual browser environment.
 require('jsdom-global')();
 
 // Setup `.vue` files to be processed by `require-extension-hooks-vue`.
